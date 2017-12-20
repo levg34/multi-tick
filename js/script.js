68478074d9c56b6eff96c878e37acaa6b371fc00
levg34 <levg34@users.noreply.github.com>
2017-12-20 11:22:08 +0100

    working display of countdown, fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 var app = angular.module('app', [])
 
 app.controller('aCtrl', function($scope,$interval) {
-	//$scope.? = ?
+	$scope.targetList = []
+	$scope.fillList = function() {
+		$scope.targetList.push({title:'Babar',color:'success',image:'car.png',date: moment().startOf('day').add(16,'hours').add(27,'minutes')})
+	}
+	$scope.countdown = function(target) {
+		var today = moment().startOf('day')
+		var now = moment()
+		var diff = moment.utc(moment.duration(target.date.diff(now)).asMilliseconds()).format("HH:mm:ss")
+		return diff
+	}
+	$scope.fillList()
 })
